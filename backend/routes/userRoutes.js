49090bb373110e9ecf948c3bd73db41d9c7da0dd
Abhay Gupta <abhaygupta@Abhays-Mac.local>
2025-03-13 01:10:58 +0530

    Handled Not Displaying of Users with empty fields

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -8,7 +8,7 @@ router.get("/", async (req, res) => {
   if(!req.user) return res.status(401).json({message: "Unauthorised"});
   try {
     const [fetchUser] = await pool.query("SELECT * FROM users where email = ?", [req.user._json.email]);
-    const [otherUser] = await pool.query("SELECT * FROM users WHERE email NOT IN (?, ?)", [req.user._json.email, req.user._json.email]);
+    const [otherUser] = await pool.query("SELECT * FROM users WHERE NOT email = ? AND NOT phone = ? AND NOT psstation = ?  AND NOT pslocation = ?", [req.user._json.email, "", "", ""]);
     res.json({success: true, user: fetchUser[0], otherUser:otherUser});
   } catch (err) {
     console.log(err);
@@ -17,8 +17,9 @@ router.get("/", async (req, res) => {
 
 router.post("/save", async (req, res) => {
   try {
-    await pool.query("UPDATE users set phone = ?, psstation = ?, pslocation = ? where email = ?", [req.body.phone, req.body.psStation, req.body.psLocation, req.user._json.email]);
-    const [check] = await pool.query("SELECT * FROM users where email = ?", [req.user._json.email]);
+    console.log(req.body);
+    await pool.query("UPDATE users set phone = ?, psstation = ?, pslocation = ? where email = ?", [req.body.phone, req.body.psStation, req.body.psLocation, req.body.email]);
+    const [check] = await pool.query("SELECT * FROM users where email = ?", [req.body.email]);
     res.json({success: true, user: check[0]});
   } catch (err) {
     console.log(err);
